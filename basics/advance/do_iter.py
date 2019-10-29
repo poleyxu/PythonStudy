f7a9b9786d45b5ba74ec759dbdd360884a804316
poleyxu <poleyxu@gmail.com>
2019-10-29 19:06:46 +0800

    modify

--- basics/advance/do_iter.py
+++ basics/advance/do_iter.py
@@ -3,13 +3,18 @@
 
 from collections import Iterable, Iterator
 
+
 def g():
     yield 1
     yield 2
     yield 3
 
+
+# list是否可迭代
 print('Iterable? [1, 2, 3]:', isinstance([1, 2, 3], Iterable))
+# str是否可迭代
 print('Iterable? \'abc\':', isinstance('abc', Iterable))
+# 整数是否可迭代
 print('Iterable? 123:', isinstance(123, Iterable))
 print('Iterable? g():', isinstance(g(), Iterable))
 
